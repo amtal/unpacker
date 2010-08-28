9dbee5032faae01e464bc72efe03b5bfc5496c77
amtal <zeiris@gmail.com>
2010-08-27 19:30:29 -0700

    a huge rewrite, and switch to package+Leksah project format happened since last commit; I need a better way to publish parts of my project while keeping the rest private...

--- Data/Bitstream.hs
+++ Data/Bitstream.hs
@@ -1,62 +1,132 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 -- |Wrapper that treats a stream of bytes as a stream of bits.
--- Simple, and reasonably efficient.
-module Data.Bitstream( Bitstream(..)
-                , stream
-                -- *Data unpackers
-                , bite, chomp
-                ) where
+module Data.Bitstream( Bitstream
+                     , runBitstream
+                     -- *Data unpackers
+                     , bite, chomp
+                     , le
+                     , streamErr
+                     ) where            
 import qualified Data.ByteString as B
+import Data.ByteString (ByteString)
 import Data.Word
 import Data.Bits
 
--- |Data type for sequentially reading bits from a ByteString.
-data Bitstream = Bitstream { bsByte :: Int
-                           , bsBit  :: Int
-                           , bsData :: B.ByteString
-                           } deriving (Eq,Show)
+import MonadLib
+import MonadLib.Monads
 
--- |Wraps a 'ByteString' in a bit reader, position set to the beginning.
-stream :: B.ByteString -> Bitstream
-stream bs = Bitstream 0 0 bs
+import Data.Maybe
 
+-- State for maintaining position within a string being parsed.
+data Place = Place { atByte :: Int
+                   , atBit  :: Int
+                   } deriving (Eq,Show)
+
+-- Immutable data used by the parser.
+data Conf = Conf { bitData :: B.ByteString
+                 } deriving (Eq,Show)
+
+newtype Bitstream a = MkStream {
+    unStream :: ReaderT Conf (StateT Place (Exception String)) a
+} deriving (Monad, Functor) -- what is this sorcery
+
+runBitstream :: ByteString -> Bitstream a -> Either String a
+runBitstream bs = fmap fst
+                . runException
+                . runStateT (Place 0 0)
+                . runReaderT (Conf bs)
+                . unStream
+
+
+-- Internal, unwrapped helper function.
+getStreamData = do
+    bs <- asks bitData
+    (Place byte bit) <- get
+    return (bs,byte,bit)
+
+-- TODO:
+-- fancy-ass debug data of
+--      bit+byte location
+--      parse history to date (positions+sizes+results) (optional?)
+-- a way to externally raise an exception
+--      parsing might look like it succeeded, but data turns out bogus
+--      in this case, want the above debug data + external exception data
+-- generalize bite and chomp to an "le" (and "be"?) that return Int(eger?)
+
+-- hmm... this needs its own range errors, and possibly its own logging
+-- TODO: lift errors to le and cstr, as well as MkStream
+-- have bite and chomp be internal, abstract things
+le :: Int -> Bitstream Int
+le n | n<=8 = fmap fromIntegral $ bite n
+     | otherwise = fmap fromIntegral $ chomp n
+
+-- | Reads a null terminated string from the stream.
+-- The null is consumed in the stream, but does not appear in the result.
+cstr :: Bitstream ByteString
+cstr = MkStream $ liftM (B.pack . takeWhile (/=0)) (unStream bs) where
+    bs :: Bitstream [Word8]
+    bs = sequence . repeat $ bite 0
+
+-- | Hmm...
+streamErr :: String -> Bitstream a
+streamErr msg = MkStream $ do
+    (Place byte bit) <- get
+    raise $ show byte++"."++show bit++": "++msg
+
+    
 -- |Reads up to a byte of data from the bitstream, moving position forward.
 --
--- Returns 'Nothing' if out of range. Number of bits to be read /must/ be 8 or less.
-bite :: Int -> Bitstream -> Maybe (Word8, Bitstream)
-bite n (Bitstream byte bit bs)
-    | n>8 = error "Bitstream.bite can only chew up to 8 bits at a time!"
-    | byte*8+bit+n > 8*B.length bs = Nothing -- out of bounds
-    | bit==0 = let
-        value = n `bitsOf` (bs `B.index` byte)
-        bitsOf b = (.&.) ((-1) `shiftR` (8-b))
-               in
-        Just (value, Bitstream (byte+((bit+n)`div`8)) ((bit+n)`mod`8) bs)
-    | otherwise = Just (value, Bitstream byte' bit' bs) where
-            value = lsbs .|. msbs
-            lsbs = ((bs `B.index` byte) `shiftR` bit) .&. ((-1) `shiftR` bit)
-            msbs = if B.length bs /= byte+1
-                then ((bs `B.index` (byte+1)) `shiftL` (8-bit)) .&. ((-1) `shiftL` (8-bit))
-                    else 0
-            byte' = byte + ((bit+n) `div` 8)
-            bit'  = (bit+n) `mod` 8
+-- Bits are read from most significant, to least significant.
+-- Raises exceptions when out of range, or when trying to read more than 8 bits. 
+bite :: Int -> Bitstream Word8
+bite 0 = MkStream $ return 0
+bite n = MkStream $ do
+    when (n>8) $ raise "bite can only chew up to 8 bits at a time!"
+    (bs,byte,bit) <- getStreamData
+    let outOfString = raise "out of bounds" -- TODO detailed stat print function
+        parsedOkay result = do
+            set $ Place (byte + ((bit+n) `div` 8)) ((bit+n) `mod` 8)
+            return result
+    maybe outOfString parsedOkay (parse byte bit bs)
+        where
+    -- Word* is unsigned, so sign-extension doesn't happen when shifting :)
+    parse :: Int -> Int -> ByteString -> Maybe Word8
+    parse byte bit bs
+        | byte*8+bit+n > 8*B.length bs = Nothing -- out of bounds
+        | bit==0 = Just $ (bs `B.index` byte) `shiftR` (8-n) 
+        | otherwise = Just $ (msbs .|. lsbs) `shiftR` (8-n) where
+            -- virtual byte spanning two physical bytes, at the current position            
+            msbs = (bs `B.index` byte) `shiftL` bit -- right side is 0-filled
+            lsbs = (bs `B.index` (byte+1)) `shiftR` (8-bit)
 -- I can see this being done more efficiently in C via loading 16 bits, then
 -- shifting. No clue how that would work in Haskell, though.
 
--- |Reads numbers with /little endian/ byte ordering.
+-- |Reads up to 32 bits with /little endian/ byte ordering.
 --
--- Returns 'Nothing' if out of range. Number of bits to be read /must/ be 32 or less.
-chomp :: Int -> Bitstream -> Maybe (Word32, Bitstream)
-chomp n stream@(Bitstream byte bit bs)
-    | n>32 = error "Bitstream.chomp can only chew up to 32 bits at a time!"
-    | byte*8+bit+n > 8*B.length bs = Nothing -- out of bounds
-    | n`mod`8/=0 = let
-        Just (seed,stream') = bite (n`mod`8) stream
-        seed :: Word8
-               in
-        loop (n-n`mod`8) stream' (fromIntegral seed)
-    | otherwise = loop n stream 0 where
-        loop :: Int -> Bitstream -> Word32 -> Maybe (Word32, Bitstream)
-        loop 0 btstr acc = Just (acc, btstr)
-        loop n btstr acc = loop (n-8) btstr' (acc`shiftL`8 .|. byte') where
-            Just (byte,btstr') = bite 8 btstr
-            byte' = fromIntegral byte
+-- Bytes are read in increasing order.
+-- Bits are read from most significant, to least significant.
+-- Raises exceptions when out of range, or trying to read more than 32 bits.
+chomp :: Int -> Bitstream Word32
+chomp 0 = MkStream $ return 0
+chomp n = MkStream $ do
+    when (n>32) $ raise "chomp can only chew up to 32 bits at a time!"
+    -- check if the parse will succeed
+    -- this way, error message can be tailored to chomp rather than bite
+    (bs,byte,bit) <- getStreamData
+    when (byte*8+bit+n > 8*B.length bs) $ raise "out of string error"
+    -- parse byte by byte
+    head <- unStream $ bite (n `mod` 8)
+    bytes <- replicateM (n `div` 8) (unStream $ bite 8)
+    -- combine bytes into word32
+    let comps = map fromIntegral (head:bytes)
+        comps :: [Word32]
+    return . fromIntegral . fst . foldl mash (0,0) $ comps
+        where
+    mash (  _,0) val = (val,8)
+    mash (acc,n) val = (acc .|. (val `shiftL` n), n+8)
+
+
+
+
+
+
